2a0702757a6382815c2f92276a4388568431e2ae
HARSH VISHWAKARMA <harshvishwakarma111@gmail.com>
2022-08-23 13:44:34 +0530

    layout component done

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
-const Layout = () => {
-  return <div>Layout</div>;
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>My store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
