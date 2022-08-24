1c4974abc52bff70730ab56a68cb31bae8d4a1be
HARSH VISHWAKARMA <harshvishwakarma111@gmail.com>
2022-08-24 11:58:49 +0530

    cart opening closing done

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,7 +1,34 @@
-import React from "react";
-
+import Link from "next/link";
+import React, { useRef } from "react";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import toast from "react-hot-toast";
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/clients";
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext();
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">{totalQuantities} items</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
