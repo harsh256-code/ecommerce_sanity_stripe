075cb6af8bf1c2a060bab372f7519984911edfd5
HARSH VISHWAKARMA <harshvishwakarma111@gmail.com>
2022-08-24 13:10:08 +0530

    cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -26,6 +26,69 @@ const Cart = () => {
           <span className="heading">Your Cart</span>
           <span className="cart-num-items">{totalQuantities} items</span>
         </button>
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button
+                type="button"
+                className="btn"
+                onClick={() => setShowCart(false)}
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item, index) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>{item.price} ₹</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span className="minus" onClick="">
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num" onClick="">
+                          0
+                        </span>
+                        <span className="plus" onClick="">
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+                    <button type="button" className="remove-item" onClick="">
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal:</h3>
+              <h3>{totalPrice} ₹</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick="">
+                Pay with Stripe
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
