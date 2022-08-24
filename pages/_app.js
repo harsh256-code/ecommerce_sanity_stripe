88c431868390d637a5219fc9357dad329205e9dd
HARSH VISHWAKARMA <harshvishwakarma111@gmail.com>
2022-08-24 11:25:33 +0530

    add to cart sucess

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 import { Layout } from "../components";
+import { StateContext } from "../context/StateContext";
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
